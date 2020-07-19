1b849335f32112007824e2a27c5679abd8f4a4e1
NihithReddy <sainihith9618@gmail.com>
2020-07-19 17:23:36 +0530

    committed

---
+++ CF709-D2-A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define repk(i,a,b,k) for(int i=(a);i<(b);i+=k)
+#define forn(i,n) for(int i=(a);i<(b);i++)
+#define all(c) (c).begin(),(c).end()
+#define clear(c) (c).clear()
+#define pb push_back
+#define ff first
+#define ss second
+#define endl "\n"
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+int main(){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        int n,b,d,a,c=0,e=0;
+        cin>>n>>b>>d;
+        rep(i,0,n){
+            cin>>a;
+            if(a<=b) c+=a;
+            if(c>d){
+                c=0;
+                e++;
+            }
+        }
+        cout<<e<<"\n";
+	return 0;
+}
