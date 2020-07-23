7103aa70ec52f2b03a5f54930284dcbf85da2a6d
NihithReddy <sainihith9618@gmail.com>
2020-07-23 20:08:00 +0530

    committed

---
+++ CF766-D2-A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define repk(i,a,b,k) for(int i=(a);i<(b);i+=k)
+#define forn(i,n) for(int i=(a);i<(b);i++)
+#define all(c) (c).begin(),(c).end()
+#define clear(c) (c).clear()
+#define pb push_back
+#define ff first
+#define ss second
+#define endl "\n"
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+int main(){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);cout.tie(0);
+        string a,b;
+        cin>>a>>b;
+        if(a==b) cout<<"-1";
+        else cout<<max(a.size(),b.size());
+	return 0;
+}
