2b271bde259d8f9af9fd3c160c2a7051c6791a85
Nihith Reddy <sainihith9618@gmail.com>
2020-08-28 17:06:44 +0530

    committed

---
+++ CF1-D12-A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define forn(i,n) for(int i=(0);i<(n);i++)
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define all(c) (c).begin(),(c).end()
+#define sz(c) (int)(c).size()
+#define pb push_back
+#define ff first
+#define ss second
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<double> vd;
+typedef vector<string> vs;
+int main(){
+	ll n,m,a;
+	cin>>n>>m>>a;
+	ll r=(n/a)+(n%a>=1);
+    ll c=(m/a)+(m%a>=1);
+	cout<<r*c<<endl;
+	return 0;
+}
