faa8aac9e4df1c133735be3127a70823f3a633a1
Nihith Reddy <sainihith9618@gmail.com>
2020-08-28 22:33:52 +0530

    committed

---
+++ CF483-D2-A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define forn(i,n) for(int i=(0);i<(n);i++)
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define all(c) (c).begin(),(c).end()
+#define sz(c) (int)(c).size()
+#define pb push_back
+#define ff first
+#define ss second
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<double> vd;
+typedef vector<string> vs;
+void solve(){
+	ll l,r;
+	cin>>l>>r;
+	while(l%2!=0) l++;
+	if((r-l)<2) cout<<-1;
+	else cout<<l<<" "<<l+1<<" "<<l+2;
+}
+int main(){
+	solve();
+	return 0;
+}
